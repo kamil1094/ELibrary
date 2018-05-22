28f18c10f588e0736220bc122f381dea36c115b6
Kamil Ciuła <kamilciula@gmail.com>
2018-05-22 13:30:52 +0200

    remote repo added

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": 0,
+        "comma-dangle": 0,
+        "object-curly-spacing": 0,
+        "indent": 0
+    }
+};
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const bookRouter = express.Router();
+
+function router(nav) {
+    const books = [{
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Miserables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
+        },
+        {
+            title: 'The Time Machine',
+            genre: 'Science Fiction',
+            author: 'H. G. Wells',
+            read: false
+        }
+    ];
+
+    bookRouter.route('/')
+        .get((req, res) => {
+            res.render('bookListView', {
+                nav,
+                title: 'ELibrary',
+                books // shorthand if variable has the same name as property
+            });
+        });
+
+    bookRouter.route('/:id')
+        .get((req, res) => {
+            const {
+                id
+            } = req.params;
+            res.render('bookView', {
+                nav,
+                title: 'ELibrary',
+                book: books[id]
+            });
+        });
+    return bookRouter;
+}
+
+module.exports = router;
